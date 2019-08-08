48c796d7322cb04df10be6bb6892448541c84caa
liheng1015 <53298133+liheng1015@users.noreply.github.com>
2019-08-07 19:40:05 -0500

    开启vsftp上传功能

---
+++ vsftpd_down.sh
@@ -0,0 +1,7 @@
+######################################开启vsftp的上传功能################################
+#!/bin/bash
+yum -y install vsftpd &> /dev/dull
+sed -i 's/^#anon_u/anon_u/' /etc/vsftpd/vsftpd.conf
+systemctl restart vsftpd
+systemctl enable vsftpd
+chmod o+w /var/ftp/pub
